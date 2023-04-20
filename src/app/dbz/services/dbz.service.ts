157df3081642573c53d9b8067fe80b5b7ad10a9f
Fernando Velazquez <fernandovelaz96@gmail.com>
2023-04-20 15:38:52 -0600

    Docs creado

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from "uuid";
+
+@Injectable({ providedIn: 'root' })
+export class DbzService {
+  public characteres: Character[] = [
+    {
+      id: uuid(),
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      id: uuid(),
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      id: uuid(),
+      name: 'Vegeta',
+      power: 7500,
+    }
+  ];
+
+  addCharacter(character: Character): void {
+    const newCharacter: Character = { id: uuid(), ...character };
+    this.characteres.push(newCharacter);
+  }
+
+  // onDeletedCharacter(index: number): void {
+  //   this.characteres.splice(index, 1);
+  // }
+
+  deleteCharacterById(id: string): void {
+    this.characteres = this.characteres.filter(character => character.id !== id);
+  }
+}
